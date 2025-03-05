58cf6bd61890cb3de78b1c57ca760a9278341732
Ferran <ftena@elpuig.xeill.net>
2025-03-05 17:02:36 +0100

    Second commit

--- src/EX1/Helloworld.java
+++ src/EX1/Helloworld.java
@@ -3,5 +3,22 @@ package EX1;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.print("Hello world");
+        
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+
+    private static void method4() {
+    }
+
+    private static void method3() {
+    }
+
+    private static void method2() {
+    }
+
+    private static void method1() {
     }
 }
